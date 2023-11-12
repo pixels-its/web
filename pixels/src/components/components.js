5f0419bc552d0b979d45cfda920146c073f67539
elopezwembii <137637051+elopezwembii@users.noreply.github.com>
2023-11-11 22:38:55 -0300

    Finalized landing

--- pixels/src/components/components.js
+++ pixels/src/components/components.js
@@ -1,6 +1,7 @@
 import { BoostrapLink } from "./bootstrap/bootstrap-link.component";
 import { Brands } from "./brands/brands.component";
 import { Card } from "./card/card.component";
+import { Footer } from "./footer/Footer.component";
 import { Link } from "./link/link.component";
 import { Navbar } from "./navbar/navbar.component";
 
@@ -8,4 +9,5 @@ Card.register("px-card", Card);
 Navbar.register("px-navbar", Navbar);
 Brands.register("px-brands", Brands);
 BoostrapLink.register("px-bootstrap", BoostrapLink);
+Footer.register("px-footer", Footer);
 Link.register("px-link", Link);
