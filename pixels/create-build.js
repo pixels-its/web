c6eed3a49b9f0b2705f3b4c9407269d4d172c0b6
elopezwembii <137637051+elopezwembii@users.noreply.github.com>
2023-11-17 18:03:28 -0300

    config export script

--- pixels/create-build.js
+++ pixels/create-build.js
@@ -1,4 +1,4 @@
-const { exec } = require("child_process");
+import { exec } from "child_process";
 
 exec("mkdir build", (error, stdout, stderr) => {
   if (error) {
@@ -8,14 +8,17 @@ exec("mkdir build", (error, stdout, stderr) => {
   console.log(`Resultado: ${stdout}`);
   console.error(`Errores: ${stderr}`);
 });
-exec("cp -r ./index.html ./build/index.html", (error, stdout, stderr) => {
-  if (error) {
-    console.error(`Error al ejecutar el comando: ${error}`);
-    return;
+exec(
+  `rsync -av --exclude='vite.config.js' --exclude='.gitignore'--exclude='package.json' --exclude='package-lock.json' --exclude='node_modules' ./ build/`,
+  (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error al ejecutar el comando: ${error}`);
+      return;
+    }
+    console.log(`Resultado: ${stdout}`);
+    console.error(`Errores: ${stderr}`);
   }
-  console.log(`Resultado: ${stdout}`);
-  console.error(`Errores: ${stderr}`);
-});
+);
 exec("cp -r ./src ./build/src", (error, stdout, stderr) => {
   if (error) {
     console.error(`Error al ejecutar el comando: ${error}`);
